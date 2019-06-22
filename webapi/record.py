deee222b612332305e89e9e98553a3b81c5b584e
howmuch515 <another.face0515@gmail.com>
2019-06-22 23:19:37 +0900

    :sparkles: format /api/v1/record

--- webapi/record.py
+++ webapi/record.py
@@ -8,11 +8,33 @@ db = DBManager()
 # select * from record
 @record.route('/record', methods=['GET'])
 def getRecord():
-    sql = "SELECT * FROM {table}".format(table=db.record_table)
+    sql = """
+            SELECT R.id AS record_id, DATE_FORMAT(R.date,'%Y-%m-%d') AS date, R.count AS count, M.id AS menu_id, M.name AS menu_name, C.id AS category_id, C.name AS category_name
+            FROM {record_table} R 
+            JOIN {menu_table} M ON R.menu_id=M.id 
+            JOIN {category_table} C ON M.category_id=C.id
+        """.format(record_table=db.record_table, menu_table=db.menu_table, category_table=db.category_table)
+
     db.cur.execute(sql)
     db.conn.commit()
     db_result = db.cur.fetchall()
-    return jsonify(db_result)
+
+    # json format
+    a = lambda x: {
+        "record_id": x["record_id"],
+        "date": x["date"],
+        "count": x["count"],
+        "menu": {
+            "menu_id": x["menu_id"],
+            "menu_name": x["menu_name"],
+            "category": {
+                "category_id": x["category_id"],
+                "category_name": x["category_name"]
+            }
+        }
+    }
+
+    return jsonify(list(map(a, db_result)))
 
 
 # insert into record...

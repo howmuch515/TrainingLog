4bb499830d9a62ac3a6e9ac3b4da849f182022fc
howmuch515 <another.face0515@gmail.com>
2019-06-23 12:45:17 +0900

    :gift: top page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from webapi.record import record
 from webapi.menu import menu
+from webui.pages import pages
 
 app = Flask(__name__)
 # /api/v1/record
@@ -8,3 +9,6 @@ app.register_blueprint(record)
 
 # /api/v1/menu
 app.register_blueprint(menu)
+
+# /*
+app.register_blueprint(pages)
---
+++ webui/pages.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+import logging
+from module.DBManager import DBManager
+
+pages = Blueprint('pages', __name__, url_prefix='/')
+db = DBManager()
+logging.basicConfig(level=logging.DEBUG)
+
+
+# top page
+@pages.route("/", methods=['GET'])
+def index():
+    name = 'Taro'
+    return render_template('index.html', name=name)

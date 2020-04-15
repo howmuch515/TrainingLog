793cb6e34c9f55d82707c255bf0bd2a096d9b518
howmuch515 <another.face0515@gmail.com>
2020-04-15 13:04:41 +0000

    :sparkles: install axios.

--- webapp/static/js/main_vue.js
+++ webapp/static/js/main_vue.js
@@ -1,8 +1,28 @@
+const MENU_API = '/api/v1/menu'
+const RECORD_API = '/api/v1/record'
+
 var vm = new Vue({
-    delimiters: ['<%=', '%>'],
+    el: "#app",
     data : function() {
         return {
-            a: 1
+            menu_list: [],
+            record_list: []
         }
-    }
+    },
+    mounted: function() {
+        axios
+        .get(MENU_API)
+        .then(res => this.menu_list = res.data)
+        .catch(err => console.error(`[-] Miss to get menu. ${err}`))
+
+        axios
+        .get(RECORD_API)
+        .then(res => this.record_list = res.data)
+        .catch(err => console.error(`[-] Miss to get record. ${err}`))
+
+        // debug
+        console.log(`[+] menu_list. ${this.menu_list}`)
+        console.log(`[+] record_list. ${this.record_list}`)
+    },
+    delimiters: ['<%=', '%>']
 })

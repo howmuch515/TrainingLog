146b4201526020c7cbf158101aecd1665adb11ee
howmuch515 <another.face0515@gmail.com>
2020-04-18 17:05:39 +0000

    :gift: complete modal list.

--- webapp/static/js/main_vue.js
+++ webapp/static/js/main_vue.js
@@ -1,157 +1,143 @@
+Vue.use(Vuex)
+
 const MENU_API = '/api/v1/menu'
 const RECORD_API = '/api/v1/record'
 
 
+const store = new Vuex.Store({
+    state: {
+        menu_list: [],
+        record_list: [],
+        modal_content: [],
+        card_category: "",
+    },
+    mutations: {
+        set_menu(state, menu_list) {
+            state.menu_list = menu_list
+        },
+        set_record(state, record_list) {
+            state.record_list = record_list
+        },
+        set_modal(state, modal_content) {
+            state.modal_content = modal_content
+        },
+        set_category(state, card_category) {
+            state.card_category = card_category
+        }
+    }
+})
+
+
 Vue.component('menu_card',{
     props: ['card_title', 'img_path'],
-    data: function() {
-        return {
+    computed: {
+        modal_content() {
+            return store.state.modal_content
         }
     },
     methods: {
-        open_modal: function(card_title) {
-            console.log(`1 ${this.record_list}`)
-            switch (this.card_title) {
-                case "pushup_card":
-                    this.current_modal_content_list = this.pushup_record_list
-                    break
-                case "squat_card":
-                    this.current_modal_content_list = this.squat_record_list
-                    break
-                case "pullup_card":
-                    this.current_modal_content_list = this.pullup_record_list
-                    break
-                case "leg_raise_card":
-                    this.current_modal_content_list = this.leg_record_list
-                    break
-                case "bridge_card":
-                    this.current_modal_content_list = this.bridge_record_list
-                    break
-                case "handstand_pushup_card":
-                    this.current_modal_content_list = this.handstand_record_list
-                    break
-                default:
-                    this.current_modal_content_list = []
-            }
-
+        open_modal: function (card_title) {
+            store.commit("set_category", card_title)
+            let record_list = store.state.record_list
+            this.filter_category(record_list, card_title)
             $('#main_modal').modal('show')
+        },
+        filter_category: function (record_list, card_title) {
+            let filtered_record_list = record_list.filter(function(v) {
+                return v["menu"]["category"]["category_name"] + "_card" == card_title
+            })
+            store.commit("set_modal", filtered_record_list)
         }
     },
     template: `
-    <div class="p-2">
-        <div :id="card_title"
-            class="card category-card">
-            <div class="card-header text-center">{{ card_title }}</div>
-            <img :src="img_path" class="card-img">
-        </div>
-    </div>
-    `
+        <div class="p-2">
+            <div :id="card_title"
+                class="card category-card"
+                @click="open_modal(card_title)">
+                <div class="card-header text-center">{{ card_title }}</div>
+                <img :src="img_path" class="card-img">
+            </div>
+        </div>`
+    
 })
 
 
 Vue.component('record-modal', {
     data: function() {
         return {
-            modal_content: []
+            pre_date: ""
         }
     },
     methods: {
-        build_modal: function(category_id) {
-            
+        isHead: function(next_date) {
+            let result = this.pre_date !== next_date
+            this.pre_date = next_date
+            return result
+        }
+    },
+    computed: {
+        modal_content() {
+            return store.state.modal_content
         }
     },
     template: `
-    <!-- Modal -->
-    <div id="main_modal" class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 id="modal_title" class="modal-title">Modal Title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div><!-- /.modal-header -->
-                <div class="modal-body">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th>DATE</th>
-                            <th>MENU(STEP)</th>
-                            <th>COUNT</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="v in modal_content">
-                                <td>{{ v.date }}</td>
-                                <td>{{ v.menu.menu_name }}(STEP:{{ v.menu.menu_step }})</td>
-                                <td>{{ v.count }}</td>
+        <!-- Modal -->
+        <div id="main_modal" class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 id="modal_title" class="modal-title">Modal Title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div><!-- /.modal-header -->
+                    <div class="modal-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>DATE</th>
+                                <th>MENU(STEP)</th>
+                                <th>COUNT</th>
                             </tr>
-                        </tbody>
-                    </table>
-                </div><!-- /.modal-body -->
-                <div class="modal-footer">
-                    <!-- send log form -->
-                    <form id="put-log-form" class="form-row" action="/api/v1/record" method="put">
-                        <input id="put-date" type="date" class="col form-control">
-                        <select id="put-menu-id" class="col form-control"></select>
-                        <input id="put-count" type="number" class="col form-control">
-                        <button type="submit" class="col btn btn-primary">Submit</button>
-                    </form>
-                </div><!-- /.modal-footer -->
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    `
+                            </thead>
+                            <tbody>
+                                <tr v-for="v in modal_content" :class="{ record_head_of_date: isHead(v.date) }">
+                                    <td>{{ v.date }}</td>
+                                    <td>{{ v.menu.menu_name }}(STEP:{{ v.menu.menu_step }})</td>
+                                    <td>{{ v.count }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div><!-- /.modal-body -->
+                    <div class="modal-footer">
+                        <!-- send log form -->
+                        <form id="put-log-form" class="form-row" action="/api/v1/record" method="put">
+                            <input id="put-date" type="date" class="col form-control">
+                            <select id="put-menu-id" class="col form-control"></select>
+                            <input id="put-count" type="number" class="col form-control">
+                            <button type="submit" class="col btn btn-primary">Submit</button>
+                        </form>
+                    </div><!-- /.modal-footer -->
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->` 
 })
 
 
 var vm = new Vue({
     el: "#app",
-    data : function() {
-        return {
-            debug: 'hello',
-            menu_list: [],
-            current_modal_content_list: [],
-            record_list: [],
-            pushup_record_list: [],
-            squat_record_list: [],
-            pullup_record_list: [],
-            leg_record_list: [],
-            bridge_record_list: [],
-            handstand_record_list: []
-        }
-    },
     mounted: function() {
         axios
         .get(MENU_API)
         .then(res => {
-            this.menu_list = res.data
-
-            // debug
-            console.log(`[+] menu_list. ${this.menu_list}`)
+            store.commit("set_menu", res.data)
         })
         .catch(err => console.error(`[-] Miss to get menu. ${err}`))
 
         axios
         .get(RECORD_API)
         .then(res => {
-            this.record_list = res.data
-
-            let a = function (category_id) {
-                return function (v) {
-                    return v["menu"]["category"]["category_id"] == category_id
-                }
-            }
-
-            this.pushup_record_list = this.record_list.filter(a(1))
-            this.squat_record_list = this.record_list.filter(a(2))
-            this.pullup_record_list = this.record_list.filter(a(3))
-            this.leg_record_list = this.record_list.filter(a(4))
-            this.bridge_record_list = this.record_list.filter(a(5))
-            this.handstand_record_list = this.record_list.filter(a(6))
-
-            // debug
-            console.log(`[+] record_list. ${this.record_list}`)
+            store.commit("set_record", res.data)
         })
         .catch(err => console.error(`[-] Miss to get record. ${err}`))
     },

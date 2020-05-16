26b96bc05d03c975874c312c500b2267c9fa2da8
howmuch515 <another.face0515@gmail.com>
2020-05-16 09:36:27 +0000

    :sparkles: complete edit function.

--- static/js/main.js
+++ static/js/main.js
@@ -51,8 +51,13 @@ Vue.component('record-modal', {
             submit_date: "",
             submit_menu_id: 5,
             submit_count: "",
+
             editable_flag: false,
-            selected_record_id: 0
+            selected_record_id: 0,
+
+            update_date: "",
+            update_menu_id: null,
+            update_count: "",
         }
     },
     methods: {
@@ -100,27 +105,36 @@ Vue.component('record-modal', {
             })
         },
         update_log: function() {
+            let update_data = {
+                record_id: this.selected_record_id,
+                date: this.update_date,
+                menu_id: this.update_menu_id,
+                count: this.update_count
+            }
             
-
             // reload
             axios
-            .put(RECORD_API, submit_data)
+            .patch(RECORD_API, update_data)
             .then(res => {
                 axios
                 .get(RECORD_API)
                 .then(res => {
                     store.commit("set_record", res.data)
+                    this.editable_flag = false
                 })
                 .catch(err => console.error(`[-] Miss to get record. ${err}`))
-
             })
             .catch(err => {
                 console.error(`[!] failed submit... ${err.data}`)
             })
         },
-        switch_editable: function(record_id) {
+        switch_editable: function(record_id, update_date, update_menu_id, update_count) {
             this.editable_flag = !this.editable_flag
             this.selected_record_id = record_id
+
+            this.update_date = update_date
+            this.update_menu_id = update_menu_id
+            this.update_count = update_count
         },
         delete_record: function(record_id) {
             alert("delete" + record_id)
@@ -167,21 +181,21 @@ Vue.component('record-modal', {
                             </thead>
                             <tbody>
                                 <tr v-for="v in selected_record_list" v-if="editable_flag && selected_record_id === v.record_id" :class="{ record_head_of_date: isHead(v.date) }">
-                                    <td><input v-model="submit_date" type="date" class="col form-control"></td>
-                                    <td><select v-model="submit_menu_id" class="col form-control">
+                                    <td><input v-model="update_date" type="date" class="col form-control"></td>
+                                    <td><select v-model="update_menu_id" class="col form-control">
                                         <option v-for="v in selected_menu_list" :value="v.menu_id">
                                             {{ v.menu_name }}(STEP: {{ v.menu_step }})
                                         </option>
                                     </select></td>
-                                    <td><input v-model="submit_count" type="number" class="col form-control"></td>
-                                    <td><button @click="submit_log" class="col btn btn-primary">Submit</button></td>
+                                    <td><input v-model="update_count" type="number" class="col form-control"></td>
+                                    <td><button @click.prevent="update_log" class="col btn btn-primary">Submit</button></td>
                                     <td><button @click="switch_editable" class="col btn btn-warning">Cancel</button></td>
                                 </tr>
                                 <tr v-else>
                                     <td>{{ v.date }}</td>
                                     <td>{{ v.menu.menu_name }}(STEP:{{ v.menu.menu_step }})</td>
                                     <td>{{ v.count }}</td>
-                                    <td><button @click="switch_editable(v.record_id)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16" width="14" height="16"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 011.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"></path></svg></button></td>
+                                    <td><button @click="switch_editable(v.record_id, v.date, v.menu.menu_id, v.count)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16" width="14" height="16"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 011.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"></path></svg></button></td>
                                     <td><button @click="delete_record(v.record_id)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" width="12" height="16"><path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path></svg></button></td>
                                 </tr>
                             </tbody>
